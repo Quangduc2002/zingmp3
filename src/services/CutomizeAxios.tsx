c3555300662c4f520f15e99ef4eb2f24fdf5f4e6
Quangduc2002 <89673332+Quangduc2002@users.noreply.github.com>
2024-05-06 11:06:56 +0700

    Update CutomizeAxios.tsx

--- src/services/CutomizeAxios.tsx
+++ src/services/CutomizeAxios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:5000/zingApi",
-  // baseURL: "https://bezingmp3.vercel.app/zingApi",
+  // baseURL: "http://localhost:5000/zingApi",
+  baseURL: "https://bezingmp3.vercel.app/zingApi",
 });
 
 instance.defaults.withCredentials = false;
